6ce848301d7d3f01224cd0ed325209072f87e42b
O'loy Global <oloyedeolaniyi22@gmail.com>
2025-11-09 04:21:11 +0100

    Implement custom permissions for Book model and secure views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,24 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register, CustomLoginView, CustomLogoutView, add_book, edit_book, delete_book, admin_view, librarian_view, member_view
+from .views import (
+    list_books,
+    LibraryDetailView,
+    register,
+    CustomLoginView,
+    CustomLogoutView,
+    add_book,
+    edit_book,
+    delete_book,
+    admin_view,
+    librarian_view,
+    member_view,
+)
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('books/add/', add_book, name='add_book'),
-    path('books/edit/<int:pk>/', edit_book, name='edit_book'),
-    path('books/delete/<int:pk>/', delete_book, name='delete_book'),
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:pk>/', edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', delete_book, name='delete_book'),
     path('register/', register, name='register'),
     path('login/', CustomLoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', CustomLogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
