9de80ee9167bc2722d5b3b9462e1cf2d246ffa32
O'loy Global <oloyedeolaniyi22@gmail.com>
2025-11-16 09:27:08 +0100

    ALX: Complete bookshelf app with CustomUser, permissions, admin, and secure views

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,3 +5,10 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'publication_year']
+
+    # Example of basic input validation
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if "<" in title or ">" in title:
+            raise forms.ValidationError("Invalid characters in title.")
+        return title
