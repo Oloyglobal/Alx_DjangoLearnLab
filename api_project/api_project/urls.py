68fba710dcb83db6b56d6878be9c279105150814
O'loy Global <oloyedeolaniyi22@gmail.com>
2025-11-21 06:56:35 +0100

    Implemented DRF token authentication and added IsAuthenticated permission to BookViewSet

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -21,3 +21,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
+
+
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    # existing urls...
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+]
