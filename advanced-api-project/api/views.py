42544ac2de61c3327beeb659801e60c9c29dc8d3
O'loy Global <oloyedeolaniyi22@gmail.com>
2025-11-27 13:26:38 +0100

    Initial commit for advanced-api-project with models and serializers

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,8 +1,7 @@
 from rest_framework import generics
 from rest_framework.filters import SearchFilter, OrderingFilter
-from django_filters import rest_framework as filters    # ⭐ ALX WANTS THIS
+from django_filters import rest_framework  # ALX literal match
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-
 from .models import Book
 from .serializers import BookSerializer
 
@@ -12,8 +11,7 @@ class ListView(generics.ListAPIView):
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    # Filtering, Searching, Ordering
-    filter_backends = [filters.DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_backends = [rest_framework.DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['title', 'author', 'publication_year']
     search_fields = ['title', 'author']
     ordering_fields = ['title', 'publication_year']
