c194a94d1a6e584e110692e6bd4b2855602a64ff
O'loy Global <oloyedeolaniyi22@gmail.com>
2025-12-05 15:50:58 +0100

    django-blog update

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,9 +3,14 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import views as auth_views
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from .models import Post
 
-
+# ------------------------------
+# Authentication Views
+# ------------------------------
 def register(request):
     """
     Register a new user using an extended UserCreationForm (includes email).
@@ -45,3 +50,36 @@ def profile(request):
         'p_form': p_form
     }
     return render(request, 'blog/profile.html', context)
+
+# ------------------------------
+# Post CRUD Views (Generic Views)
+# ------------------------------
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post_list')
