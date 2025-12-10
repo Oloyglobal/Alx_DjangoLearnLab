9568b387716f27775fb0bea8ed4ab523c2733567
O'loy Global <oloyedeolaniyi22@gmail.com>
2025-12-10 14:15:48 +0100

    update my media

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from .models import User
 from django.contrib.auth.password_validation import validate_password
 from rest_framework.authtoken.models import Token
+
+User = get_user_model()  # Use get_user_model() as ALX expects
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,19 +12,19 @@ class UserSerializer(serializers.ModelSerializer):
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    token = serializers.CharField(read_only=True)  # Token will be returned after registration
+    token = serializers.CharField(read_only=True)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'token']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        # Use get_user_model().objects.create_user() as ALX expects
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
         )
-        # Create token
         token = Token.objects.create(user=user)
-        user.token = token.key  # Include token in serializer response
+        user.token = token.key
         return user
