4c23b2fc152449ba85c9aae5b9b385c61384f181
O'loy Global <oloyedeolaniyi22@gmail.com>
2025-12-12 07:06:01 +0100

    updated

---
+++ social_media_api/accounts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import follow_user, unfollow_user
+
+urlpatterns = [
+    path("follow/<int:user_id>/", follow_user, name="follow-user"),
+    path("unfollow/<int:user_id>/", unfollow_user, name="unfollow-user"),
+]
--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -24,3 +24,5 @@ urlpatterns = [
     path('api/accounts/', include('accounts.urls')),
     path('api/', include('posts.urls')),
 ]
+
+
